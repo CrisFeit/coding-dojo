ded39727f44c7e598fa1529acbb347b8bd11f47f
amandapratesc <amanda.caetano@grupoboticario.com.br>
2023-09-15 11:45:49 -0300

    main - improvements on test

--- src/unit.test.js
+++ src/unit.test.js
@@ -1,5 +1,6 @@
 const { sum1 } = require('./index.js')
 
 test('Checa se os valores estão corretos', () => {
-  expect([]).toEqual(expect.any(Array))
-  });
+  expect(sum1).toEqual(expect.any(Number));
+});
+
